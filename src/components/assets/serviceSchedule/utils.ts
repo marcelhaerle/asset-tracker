888483ef7eb258f6240b73287787bdd5e5209109
Marcel Härle <marcel.haerle@sonnvest.de>
2025-03-22 18:24:13 +0100

    Euro instead of dollars

--- src/components/assets/serviceSchedule/utils.ts
+++ src/components/assets/serviceSchedule/utils.ts
@@ -5,9 +5,9 @@ export const formatDate = (dateString: string | null): string => {
 
 export const formatCurrency = (amount: number | null): string => {
   if (amount === null) return 'Not specified';
-  return new Intl.NumberFormat('en-US', {
+  return new Intl.NumberFormat('de', {
     style: 'currency',
-    currency: 'USD',
+    currency: 'EUR',
   }).format(amount);
 };
 
